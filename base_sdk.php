99bff661af2c23f6e6226577428e09ce2cb46fa8
Eric Iheanyichukwu <chisomezenna@gmail.com>
2017-03-26 02:49:05 +0100

    Create base_sdk.php
    This file make other sdk files to stay connected

---
+++ base_sdk.php
@@ -0,0 +1,48 @@
+<?php
+/*
+* @author Eric C Iheanyichukwu (Nusky)
+*
+* @about FRIENDEYZ SDK CONNECTOR
+*/
+/*
+ * Copyright 2015 Friendeyz,Inc.
+	*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+	*
+ *     http://www.apache.org/licenses/LICENSE-2.0
+	*
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+//Main sdk 
+class base_fdz_sdk{
+     private $sdk_api_key = "your sdk key";
+     
+     private $fdz_url = "http://sdk.frdyz.net/base_class";
+     
+        protected function make_sdk($this_key,$sdk_name,$fdzstatic_btrs){
+        //call the sdk for action       
+        
+        }
+        public function connect_sdk(){
+        #do not modify this function except you know what you are doing
+           //return base url
+              if($this->sdk_api_key==null){
+                 return false;
+                 }else{
+                 /*Continue Execution of the Script*/
+                 }
+        
+        }
+//this file carries other files along
+
+
+
+}
+?>
